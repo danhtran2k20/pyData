87987156fc0165ce9a96ef6171bea6149ec337c4
danh tran <tldanh1993@gmail.com>
2021-09-22 20:52:58 +0700

    Update unfinish proj

--- zpyDS0_Project/page-view-time-series-visualizer/time_series_visualizer.py
+++ zpyDS0_Project/page-view-time-series-visualizer/time_series_visualizer.py
@@ -2,53 +2,52 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
+
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
-
+df = pd.read_csv("fcc-forum-pageviews.csv")
+df = df.set_index('date')
 # Clean data
-df = None
-
+df = df = df[
+    (df["value"] > df["value"].quantile(0.025))
+    & (df["value"] < df["value"].quantile(0.975))
+]
+df.index = pd.to_datetime(df.index)
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
-
-    # Save image and return fig (don't change this part)
-    fig.savefig('line_plot.png')
-    return fig
-
-def draw_bar_plot():
-    # Copy and modify data for monthly bar plot
-    df_bar = None
-
-    # Draw bar plot
-
-
-
-
+    fig = plt.figure(figsize=(20, 6))
+    plt.plot(df.index, df["value"], color="r")
+    plt.title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019", color="black")
+    plt.xlabel("Date")
+    plt.ylabel("Page Views")
 
     # Save image and return fig (don't change this part)
-    fig.savefig('bar_plot.png')
+    fig.savefig("line_plot.png")
     return fig
+
+
+# def draw_bar_plot():
+#     # Copy and modify data for monthly bar plot
+#     df_bar = None
+
+#     # Draw bar plot
+
+#     # Save image and return fig (don't change this part)
+#     fig.savefig("bar_plot.png")
+#     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
     df_box = df.copy()
     df_box.reset_index(inplace=True)
-    df_box['year'] = [d.year for d in df_box.date]
-    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+    df_box["year"] = [d.year for d in df_box.date]
+    df_box["month"] = [d.strftime("%b") for d in df_box.date]
 
     # Draw box plots (using Seaborn)
 
-
-
-
-
     # Save image and return fig (don't change this part)
-    fig.savefig('box_plot.png')
+    fig.savefig("box_plot.png")
     return fig
--- zpyDS0_Project/sea-level-predictor/sea_level_predictor.py
+++ zpyDS0_Project/sea-level-predictor/sea_level_predictor.py
@@ -4,10 +4,14 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
+    plt.figure(figsize=(12,6))
+    plt.scatter(df["Year"], df["CSIRO Adjusted Sea Level"])
+    plt.title("Rise in Sea Level")
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
 
     # Create first line of best fit
 
