d048b3322eb61fda0cbcf0c65703d45b13e683a4
danh tran <tldanh1993@gmail.com>
2021-09-23 22:34:12 +0700

    fix temp variable and finish prj3

--- zpyDS0_Project/medical-data-visualizer/medical_data_visualizer.py
+++ zpyDS0_Project/medical-data-visualizer/medical_data_visualizer.py
@@ -7,9 +7,15 @@ import numpy as np
 df = pd.read_csv("medical_examination.csv")
 
 # Add 'overweight' column
-df["BMI"] = df["weight"] / ((df["height"] / 100) ** 2)
-df["overweight"] = [1 if x > 25 else 0 for x in df["BMI"]]
+# df["BMI"] = df["weight"] / ((df["height"] / 100) ** 2)
+# df["overweight"] = [1 if x > 25 else 0 for x in df["BMI"]]
+# cannot create BMI because affect corr value for heat map
+def check_overweight(x):
+    BMI = x["weight"] / ((x["height"] / 100) ** 2)
+    return 1 if BMI > 25 else 0
 
+
+df["overweight"] = df.apply(check_overweight, axis=1)
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
 df["cholesterol"] = [0 if x == 1 else 1 for x in df["cholesterol"]]
 df["gluc"] = [0 if x == 1 else 1 for x in df["gluc"]]
@@ -34,7 +40,9 @@ def draw_cat_plot():
     fig = sns.catplot(
         data=df_cat, kind="bar", x="variable", y="total", hue="value", col="cardio"
     ).set_axis_labels("variable", "total")
+    # Cannot assign fig as heat map ? Recheck
     fig = fig.fig
+
     # Do not modify the next two lines
     fig.savefig("catplot.png")
     return fig
@@ -43,19 +51,35 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[
+        (df["ap_lo"] <= df["ap_hi"])
+        & (df["height"] >= df["height"].quantile(0.025))
+        & (df["height"] <= df["height"].quantile(0.975))
+        & (df["weight"] >= df["weight"].quantile(0.025))
+        & (df["weight"] <= df["weight"].quantile(0.975))
+    ]
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr()
 
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(10, 8))
 
     # Draw the heatmap with 'sns.heatmap()'
-
+    sns.heatmap(
+        corr,
+        mask=mask,
+        vmax=0.3,
+        center=0,
+        square=True,
+        linewidths=0.5,
+        annot=True,
+        fmt=".1f",
+        cbar_kws={"shrink": 0.5, "ticks": np.arange(-0.08, 0.25, 0.08)},
+    )
     # Do not modify the next two lines
     fig.savefig("heatmap.png")
     return fig
